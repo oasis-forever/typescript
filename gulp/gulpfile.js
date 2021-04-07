ab91025e297b9b10202b6a841f839ed86cec0b2d
Oasist <bin.lhkty.gtthj.o.zs@gmail.com>
2021-04-08 00:29:58 +0900

    Commit showGreeting

--- gulp/gulpfile.js
+++ gulp/gulpfile.js
@@ -1,6 +1,29 @@
 const gulp = require("gulp");
 const ts = require("gulp-typescript");
+const browserify = require("browserify");
+const source = require("vinyl-source-stream");
+const tsify = require("tsify");
+const paths = { pages: ["./src/*.html"] };
 const tsProject = ts.createProject("tsconfig.json");
 gulp.task("default", () => {
   return tsProject.src().pipe(tsProject()).js.pipe(gulp.dest("dist"));
 });
+gulp.task("copy-html", () => {
+  return gulp.src(paths.pages).pipe(gulp.dest("dist"));
+});
+gulp.task(
+  "default",
+  gulp.series(gulp.parallel("copy-html"), () => {
+    return browserify({
+      basedir: ".",
+      debug: true,
+      entries: ["./src/main.ts"],
+      cache: {},
+      packageCache: {},
+    })
+    .plugin(tsify)
+    .bundle()
+    .pipe(source("bundle.js"))
+    .pipe(gulp.dest("dist"));
+  })
+);
--- gulp/src/main.ts
+++ gulp/src/main.ts
@@ -1,6 +1,21 @@
+import {
+  greet
+} from "./greetings";
+
 const greetFromCompiler = (greeting: string, compiler: string) => {
   return `${greeting} from ${compiler}!!`;
 };
+
+const showGreeting = (divID: string, message: string) => {
+  const elem = document.getElementById(divID);
+  elem.innerText = message;
+};
+
+const divID = "greeting";
+const greeting = greet(1);
+const compiler = "TyepScript";
+const message = greetFromCompiler(greeting, compiler);
+showGreeting(divID, message);
 
 export {
   greetFromCompiler
