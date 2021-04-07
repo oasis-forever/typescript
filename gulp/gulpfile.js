8827582face5e5084235f91fbd0ad59050e51609
Oasist <bin.lhkty.gtthj.o.zs@gmail.com>
2021-04-08 00:57:19 +0900

    Update gulpfile.js

--- gulp/gulpfile.js
+++ gulp/gulpfile.js
@@ -1,29 +1,31 @@
 const gulp = require("gulp");
 const browserify = require("browserify");
 const source = require("vinyl-source-stream");
-const watchify = require("watchify");
 const tsify = require("tsify");
-const fancy_log = require("fancy-log");
+const uglify = require("gulp-uglify");
+const sourcemaps = require("gulp-sourcemaps");
+const buffer = require("vinyl-buffer");
 const paths = { pages: ["./src/*.html"] };
-const watchedBrowserify = watchify(
-  browserify({
-    basedir: ".",
-    debug: true,
-    entries: ["./src/main.ts"],
-    cache: {},
-    packageCache: {},
-  }).plugin(tsify)
-);
 gulp.task("copy-html", () => {
   return gulp.src(paths.pages).pipe(gulp.dest("dist"));
 });
-const bundle = () => {
-  return watchedBrowserify
-  .bundle()
-  .on("error", fancy_log)
-  .pipe(source("bundle.js"))
-  .pipe(gulp.dest("dist"));
-}
-gulp.task("default", gulp.series(gulp.parallel("copy-html"), bundle));
-watchedBrowserify.on("update", bundle);
-watchedBrowserify.on("log", fancy_log);
+gulp.task(
+  "default",
+  gulp.series(gulp.parallel("copy-html"), () => {
+    return browserify({
+      basedir: ".",
+      debug: true,
+      entries: ["src/main.ts"],
+      cache: {},
+      packageCache: {},
+    })
+    .plugin(tsify)
+    .bundle()
+    .pipe(source("bundle.js"))
+    .pipe(buffer())
+    .pipe(sourcemaps.init({ loadMaps: true }))
+    .pipe(uglify())
+    .pipe(sourcemaps.write("./"))
+    .pipe(gulp.dest("dist"));
+  })
+);
